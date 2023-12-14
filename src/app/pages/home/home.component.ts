8132c7dd3db95ab0a7526f46c3e8e3927366305f
Juan <pablohenao@saimyr.com.co>
2023-12-14 16:25:38 -0500

    Continuación curso.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, Injector, OnInit, computed, effect, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Task } from '../../models/task.model';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { state } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
@@ -10,24 +11,65 @@ import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-  task = signal<Task[]>([
+export class HomeComponent implements OnInit{
+  tasks = signal<Task[]>([
     {
       id: Date.now(),
       title: 'crear proyect',
-      completed: false
+      completed: true
     },
     {
       id: Date.now(),
       title: 'crear componentes',
       completed: false
     }
-  ])
+  ]);
+
+  filter = signal('all');
+  taskByFilter = computed(() => {
+    const filter = this.filter();
+    const tasks = this.tasks();
+
+    if (filter === 'pending') {
+      return tasks.filter(task => !task.completed)
+    }
+    if (filter === 'completed') {
+      return tasks.filter(task => task.completed)
+    }
+    return tasks;
+  })
 
   newTaskCtrl = new FormControl('', {
     nonNullable: true,
     validators: [Validators.required]
   });
+
+  injector = inject(Injector);
+
+  constructor() {
+    /* effect(() => {
+      const tasks = this.tasks();
+      console.log('run effect');
+
+    }) */
+  }
+
+  ngOnInit(): void {
+    const storage = localStorage.getItem('tasks');
+    if(storage){
+      const tasks = JSON.parse(storage);
+      this.tasks.set(tasks);
+    }
+    this.trackTasks();
+  }
+
+  trackTasks() {
+    effect(() => {
+      const tasks = this.tasks();
+      console.log(tasks);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }, { injector: this.injector })
+  }
 
   changeHandler() {
     if (this.newTaskCtrl.valid) {
@@ -45,11 +87,11 @@ export class HomeComponent {
       title,
       completed: false
     }
-    this.task.update((task) => [...task, newTask]); //crea un nuevo estado, agerga las tareas que ya tenia y al final la nueva tarea
+    this.tasks.update((task) => [...task, newTask]); //crea un nuevo estado, agerga las tareas que ya tenia y al final la nueva tarea
   }
 
   updateTask(index: any) {
-    this.task.update((task) => {
+    this.tasks.update((task) => {
       return task.map((task, position) => {
         if (position === index) {
           return {
@@ -60,9 +102,47 @@ export class HomeComponent {
         return task;
       })
     })
+
   }
 
   deleteTask(index: number) {
-    this.task.update((task) => task.filter((task, position) => position !== index))
+    this.tasks.update((task) => task.filter((task, position) => position !== index))
+  }
+
+  updateTaskEditingMode(index: number) {
+    this.tasks.update((prevState) => {
+      return prevState.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            editing: true
+          }
+        }
+        return {
+          ...task,
+          editing: false,
+        }
+      })
+    });
+  }
+
+  updateTaskText(index: number, event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.tasks.update((prevState) => {
+      return prevState.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            title: input.value,
+            editing: false
+          }
+        }
+        return task
+      })
+    });
+  }
+
+  chageFilter(filter: string) {
+    this.filter.set(filter)
   }
 }
